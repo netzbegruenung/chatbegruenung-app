0abd4676ff25d2191cdbbc4ff39158ecf2454448
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-05 13:47:00 +0200

    Whitelabel: Whitelabel iOS

--- ios/RocketChatRN/AppDelegate.mm
+++ ios/RocketChatRN/AppDelegate.mm
@@ -23,7 +23,7 @@
   [RNNotifications startMonitorNotifications];
   [ReplyNotification configure];
 
-  self.moduleName = @"RocketChatRN";
+  self.moduleName = @"chatbegrünung";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
