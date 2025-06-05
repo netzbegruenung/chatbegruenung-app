8e0e24fc3240852be52e8f4c721aef468dab8a6e
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-05 14:28:13 +0200

    Whitelabel: Remove firebase analytics and crashlytics

--- app/lib/methods/helpers/log/index.ts
+++ app/lib/methods/helpers/log/index.ts
@@ -1,13 +1,11 @@
-import analytics from '@react-native-firebase/analytics';
-import crashlytics from '@react-native-firebase/crashlytics';
 import bugsnag from '@bugsnag/react-native';
 
 import events from './events';
 
 export { events };
 
-let reportCrashErrors = true;
-let reportAnalyticsEvents = true;
+const reportCrashErrors = false;
+const reportAnalyticsEvents = false;
 
 export const getReportCrashErrorsValue = (): boolean => reportCrashErrors;
 export const getReportAnalyticsEventsValue = (): boolean => reportAnalyticsEvents;
@@ -34,7 +32,6 @@ export const logServerVersion = (serverVersion: string): void => {
 
 export const logEvent = (eventName: string, payload?: { [key: string]: any }): void => {
 	try {
-		analytics().logEvent(eventName, payload);
 		bugsnag.leaveBreadcrumb(eventName, payload);
 	} catch {
 		// Do nothing
@@ -42,26 +39,18 @@ export const logEvent = (eventName: string, payload?: { [key: string]: any }): v
 };
 
 export const setCurrentScreen = (currentScreen: string): void => {
-	analytics().logScreenView({ screen_class: currentScreen, screen_name: currentScreen });
 	bugsnag.leaveBreadcrumb(currentScreen, { type: 'navigation' });
 };
 
-export const toggleCrashErrorsReport = (value: boolean): boolean => {
-	crashlytics().setCrashlyticsCollectionEnabled(value);
-	return (reportCrashErrors = value);
-};
+export const toggleCrashErrorsReport = (_: boolean): boolean => false;
 
-export const toggleAnalyticsEventsReport = (value: boolean): boolean => {
-	analytics().setAnalyticsCollectionEnabled(value);
-	return (reportAnalyticsEvents = value);
-};
+export const toggleAnalyticsEventsReport = (_: boolean): boolean => false;
 
 const log = (e: any): void => {
 	if (e instanceof Error && bugsnag && e.message !== 'Aborted' && !__DEV__) {
 		bugsnag.notify(e, (event: { addMetadata: (arg0: string, arg1: {}) => void }) => {
 			event.addMetadata('details', { ...metadata });
 		});
-		crashlytics().recordError(e);
 	} else {
 		console.error(e);
 	}
--- app/views/SecurityPrivacyView.tsx
+++ app/views/SecurityPrivacyView.tsx
@@ -1,33 +1,21 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import * as List from '../containers/List';
 import SafeAreaView from '../containers/SafeAreaView';
 import StatusBar from '../containers/StatusBar';
 import I18n from '../i18n';
-import { ANALYTICS_EVENTS_KEY, CRASH_REPORT_KEY } from '../lib/constants';
 import { useAppSelector } from '../lib/hooks';
 import useServer from '../lib/methods/useServer';
 import { SettingsStackParamList } from '../stacks/types';
 import { handleLocalAuthentication } from '../lib/methods/helpers/localAuthentication';
-import {
-	events,
-	getReportAnalyticsEventsValue,
-	getReportCrashErrorsValue,
-	logEvent,
-	toggleAnalyticsEventsReport,
-	toggleCrashErrorsReport
-} from '../lib/methods/helpers/log';
-import Switch from '../containers/Switch';
+import { events, logEvent } from '../lib/methods/helpers/log';
 
 interface ISecurityPrivacyViewProps {
 	navigation: NativeStackNavigationProp<SettingsStackParamList, 'SecurityPrivacyView'>;
 }
 
 const SecurityPrivacyView = ({ navigation }: ISecurityPrivacyViewProps): JSX.Element => {
-	const [crashReportState, setCrashReportState] = useState(getReportCrashErrorsValue());
-	const [analyticsEventsState, setAnalyticsEventsState] = useState(getReportAnalyticsEventsValue());
 	const [server] = useServer();
 
 	const e2eEnabled = useAppSelector(state => state.settings.E2E_Enable);
@@ -37,20 +25,6 @@ const SecurityPrivacyView = ({ navigation }: ISecurityPrivacyViewProps): JSX.Ele
 			title: I18n.t('Security_and_privacy')
 		});
 	}, [navigation]);
-
-	const toggleCrashReport = (value: boolean) => {
-		logEvent(events.SP_TOGGLE_CRASH_REPORT);
-		AsyncStorage.setItem(CRASH_REPORT_KEY, JSON.stringify(value));
-		setCrashReportState(value);
-		toggleCrashErrorsReport(value);
-	};
-
-	const toggleAnalyticsEvents = (value: boolean) => {
-		logEvent(events.SP_TOGGLE_ANALYTICS_EVENTS);
-		AsyncStorage.setItem(ANALYTICS_EVENTS_KEY, JSON.stringify(value));
-		setAnalyticsEventsState(value);
-		toggleAnalyticsEventsReport(value);
-	};
 
 	const navigateToScreen = (screen: 'E2EEncryptionSecurityView' | 'ScreenLockConfigView') => {
 		// @ts-ignore
@@ -89,25 +63,6 @@ const SecurityPrivacyView = ({ navigation }: ISecurityPrivacyViewProps): JSX.Ele
 						testID='security-privacy-view-screen-lock'
 					/>
 					<List.Separator />
-				</List.Section>
-
-				<List.Section>
-					<List.Separator />
-					<List.Item
-						title='Log_analytics_events'
-						testID='security-privacy-view-analytics-events'
-						right={() => <Switch value={analyticsEventsState} onValueChange={toggleAnalyticsEvents} />}
-						additionalAcessibilityLabel={analyticsEventsState}
-					/>
-					<List.Separator />
-					<List.Item
-						title='Send_crash_report'
-						testID='security-privacy-view-crash-report'
-						right={() => <Switch value={crashReportState} onValueChange={toggleCrashReport} />}
-						additionalAcessibilityLabel={analyticsEventsState}
-					/>
-					<List.Separator />
-					<List.Info info='Crash_report_disclaimer' />
 				</List.Section>
 			</List.Container>
 		</SafeAreaView>
