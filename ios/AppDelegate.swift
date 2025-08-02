51fc0a3421a283dd4d9270a2acfc2f7635eaf4be
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-08-02 15:18:35 +0200

    Whitelabel: Whitelabel iOS

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -37,7 +37,7 @@ public class AppDelegate: ExpoAppDelegate {
 #if os(iOS) || os(tvOS)
     window = UIWindow(frame: UIScreen.main.bounds)
     factory.startReactNative(
-      withModuleName: "RocketChatRN",
+      withModuleName: "chatbegrünung",
       in: window,
       launchOptions: launchOptions)
 #endif
