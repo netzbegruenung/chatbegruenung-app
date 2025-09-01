5e8d2ed29d779e5a95d0dbbb7eccd68420d612f6
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-01 11:10:48 +0200

    Whitelabel: Configure deep linking

--- app/index.tsx
+++ app/index.tsx
@@ -60,7 +60,7 @@ interface IState {
 
 const parseDeepLinking = (url: string) => {
 	if (url) {
-		url = url.replace(/rocketchat:\/\/|https:\/\/go.rocket.chat\//, '');
+		url = url.replace(/rocketchat:\/\/|https:\/\/chatbegruenung.de\//, '');
 		const regex = /^(room|auth|invite|shareextension)\?/;
 		const match = url.match(regex);
 		if (match) {
