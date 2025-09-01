15efc5d815a2c746c1ab745a57bcff0d55b000d5
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-01 11:10:48 +0200

    Whitelabel: Configure deep linking

--- app/lib/methods/helpers/parseDeepLinking.ts
+++ app/lib/methods/helpers/parseDeepLinking.ts
@@ -2,7 +2,7 @@ import parseQuery from './parseQuery';
 
 const parseDeepLinking = (url: string) => {
 	if (url) {
-		url = url.replace(/rocketchat:\/\/|https:\/\/go.rocket.chat\//, '');
+		url = url.replace(/rocketchat:\/\/|https:\/\/chatbegruenung.de\//, '');
 		const regex = /^(room|auth|invite|shareextension)\?/;
 		const match = url.match(regex);
 		if (match) {
@@ -16,6 +16,9 @@ const parseDeepLinking = (url: string) => {
 					type: matchedPattern === 'shareextension' ? matchedPattern : parsedQuery?.type
 				};
 			}
+		}
+		if (/^(invite|channel|direct|group)\//.test(url)) {
+			return { host: 'chatbegruenung.de', path: url };
 		}
 	}
 
